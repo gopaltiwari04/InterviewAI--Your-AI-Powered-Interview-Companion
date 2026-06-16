5f2db41034bca8f771302cb5dee59cfb16fa48fc
Gopal Tiwari <gopal23223@iiitd.ac.in>
2026-06-16 21:10:10 +0530

    remove explicit secret from middleware

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 export default async function middleware(req: NextRequest) {
   const token = await getToken({
     req,
-    secret: process.env.AUTH_SECRET,
+    //secret: process.env.AUTH_SECRET,
   });
 
   const isLoggedIn = !!token;
